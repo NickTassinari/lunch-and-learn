63c568e1f9e8f6b1e72d6058f7aa6d415addfc13
Nick Tassinari <ntass7800@gmail.com>
2023-08-06 10:15:55 -0600

    test country facade class methods

--- spec/facades/country_facade_spec.rb
+++ spec/facades/country_facade_spec.rb
@@ -6,9 +6,29 @@ RSpec.describe CountryFacade do
       it "returns a String of the common name of randomly selected country", :vcr do 
         expect(CountryFacade.random_country).to be_a(String)
       end
-
-      
     end
     
+    describe ".is_a_country" do 
+      it "returns a string with valid params", :vcr do
+        expect(CountryFacade.is_a_country("ital")).to be_a(String)
+        expect(CountryFacade.is_a_country("ital")).to eq("Italy")
+      end
+
+      it "returns nil with invalid params", :vcr do 
+        expect(CountryFacade.is_a_country("Nicklan")).to be_a(NilClass)
+        expect(CountryFacade.is_a_country("")).to be_a(NilClass)
+      end
+    end
+
+    describe ".results" do 
+      it "returns array of countries matching param", :vcr do 
+        expect(CountryFacade.results("ital")).to be_a(Hash)
+        expect(CountryFacade.results("ital")).to have_key(:name)
+        expect(CountryFacade.results("ital")[:name]).to be_a(Hash)
+        expect(CountryFacade.results("ital")[:name]).to have_key(:common)
+        expect(CountryFacade.results("ital")[:name][:common]).to be_a(String)
+        expect(CountryFacade.results("ital")[:name][:common]).to eq("Italy")
+      end
+    end
   end
 end
